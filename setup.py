6c3fba10edef43ea64d064ce3b2b30059e162c13
Eduardo Elias <camponez@gmail.com>
2021-03-21 13:18:39 +0000

    New setup.py parameters

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f_file:
     long_description = f_file.read()
 
+extras = {
+    "test": [
+        "pytest >=2.7.3",
+    ]
+}
+
 
 setuptools.setup(
     name="juungle",
@@ -13,19 +19,24 @@ setuptools.setup(
     description="Juungle python package for juungle.net",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="juungle.net api nfts",
     url="https://github.com/Juungle/juungle-python",
     project_urls={
         "Bug Tracker": "https://github.com/Juungle/juungle-python/issues",
+        "Source Code": "https://github.com/Juungle/juungle-python",
     },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Utilities",
     ],
     install_requires=[
         'requests'
     ],
+    extras_require=extras,
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
