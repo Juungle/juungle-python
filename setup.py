d0ed37599031d6dea09093bda7c19a2ef5f7cc48
Eduardo Elias <camponez@gmail.com>
2021-03-22 18:17:08 +0000

    v0.6.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extras = {
 
 setuptools.setup(
     name="juungle",
-    version="0.5.1",
+    version="0.6.0",
     author="Eduardo Elias",
     author_email="camponez@gmail.com",
     description="Juungle python package for juungle.net",
